5795334f1230a01f492725ef7995249de2707e8e
Riccardo <ric.menon2000@gmail.com>
2021-07-12 17:51:05 +1000

    done boiiiiiiiiii

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-// viewed at http://localhost:3000
 app.get("/", function(req, res) {
-  res.send("Tic Tac Toe");
+    res.sendFile(__dirname + "/resources/html/index.html");
 });
 
-app.listen(port, () => console.log(`Running app listening on port ${port}!`));
+var blue_player = null;
+var red_player = null;
+var current_turn = "red";
+
+io.on('connection', (socket) => {
+    
+    if (red_player == null) {
+        console.log('red player connected');
+        socket.emit("assign-team", "red");
+        red_player = socket.id;
+    } else if (blue_player == null) {
+        console.log('blue player connected');
+        socket.emit("assign-team", "blue");
+        blue_player = socket.id;
+    } else {
+        console.log('spectator connected');
+        socket.emit("assign-team", "spectator");
+    }
+
+    socket.on('action', (data) => {
+        current_turn = current_turn == "red" ? "blue" : "red"
+        data.current_turn = current_turn;
+        io.emit('action', data);
+    });
+
+    socket.on('disconnect', () => {
+
+        if (socket.id == red_player) {
+            console.log('red player left')
+            red_player = null;
+            io.emit('reset');
+        } else if (socket.id == blue_player) {
+            console.log('blue player left')
+            blue_player = null;
+            io.emit('reset');
+        } else {
+            console.log('spectator left')
+        }
+    })
+
+    if ((red_player != null && blue_player != null) && (socket.id == blue_player || socket.id == red_player)) {
+        current_turn = Math.random() < 0.5 ? "red" : "blue";
+        io.emit('start', {
+            current_turn: current_turn
+        });
+    }
+
+});
+
+server.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
